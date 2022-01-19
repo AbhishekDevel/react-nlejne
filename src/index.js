a26a5d6c262fd629df4d063c1718554f9e309e1a
AbhishekDevel <abhishek947122@gmail.com>
2022-01-19 19:07:58 +0530

    1 file modified

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style.css';
+class Board extends React.Component{
+  render(){
+    const status = "Next Player is X"
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">        {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+class Game extends React.Component{
+   render(){
+     return(
+     <div className="game">
+       <div className="game-board">
+       </div>
+       <Board />
+       <div className="game-info">
+         <div>{/* status */}</div>
+         <ol>{/**/}</ol>
+       </div>
+     </div>
+     )
+   }
+}
+ReactDom.render(<Game />, document.getElementById('root'))
